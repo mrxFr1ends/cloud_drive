09010ae0db22932bcfdeb9e2c8262740ff380683
mrx_fr1ends <prosto.pasha123@gmail.com>
2023-08-03 12:36:08 +0300

    Fix return error message problem

--- src/middlewares/serverErrorMiddleware.js
+++ src/middlewares/serverErrorMiddleware.js
@@ -1,5 +1,6 @@
-export const serverErrorMiddleware = (err, req, res) => {
-    console.log(err);
+// eslint-disable-next-line no-unused-vars
+export const serverErrorMiddleware = (err, req, res, next) => {
+    console.error(err);
     res.status(err.statusCode || 500).send({
         message: err.statusCode ? err.message : "Internal Server Error",
     });
