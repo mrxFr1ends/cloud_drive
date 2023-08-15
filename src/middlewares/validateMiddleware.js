2f001b3b137009213e3a06ba6275117c19c28b03
mrx_fr1ends <prosto.pasha123@gmail.com>
2023-08-15 12:12:49 +0300

    Change validateMiddleware. Added set only valid fields

--- src/middlewares/validateMiddleware.js
+++ src/middlewares/validateMiddleware.js
@@ -1,12 +1,18 @@
-import { validationResult } from "express-validator";
+import { matchedData, validationResult } from "express-validator";
 
 export const validateMiddleware = validationRules => {
     return async (req, res, next) => {
         await Promise.all(validationRules.map(rule => rule.run(req)));
-
+        
         const errors = validationResult(req);
-        if (errors.isEmpty())
+        if (errors.isEmpty()) {
+            req.body = matchedData(req, { locations: ['body'], includeOptionals: true });
+            req.query = matchedData(req, { locations: ['query'], includeOptionals: true });
+            req.params = matchedData(req, { locations: ['params'], includeOptionals: true });
+            req.cookies = matchedData(req, { locations: ['cookies'], includeOptionals: true });
+            req.headers = matchedData(req, { locations: ['headers'], includeOptionals: true });
             return next();
+        }
 
         res.status(400).json({
             message: "Validation failed",
